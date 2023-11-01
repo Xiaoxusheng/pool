c504ce8773344860b274fdd5150d2d8173748d1c
lei <3096407768@qq.com>
2023-11-01 20:42:46 +0800

    解决bug

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ func main() {
 	ctx := context.Background()
 
 	for i := 0; i < 10; i++ {
+		i = i
 		err := pool.Submit(ctx, func(v ...interface{}) {
 			log.Println(i)
 			add(1, 2, i)
-			fmt.Println("第" + strconv.Itoa(i) + "个任务执行完成")
 		})
 		if err != nil {
 			log.Println(err)
